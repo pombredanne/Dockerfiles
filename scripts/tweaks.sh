60c978f8db93d3e0096c69730260f7b55f428241
fdiskyou <rui@deniable.org>
2018-04-10 22:16:53 +0100

    remove

--- scripts/tweaks.sh
+++
@@ -1,6 +0,0 @@
-echo core > /proc/sys/kernel/core_pattern
-echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-ulimit -u unlimited
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-echo 1 >/proc/sys/kernel/sched_child_runs_first
-echo 1 >/proc/sys/kernel/sched_autogroup_enabled 
